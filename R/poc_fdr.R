3b643aa851b7e904fe18590b111caca2a2b08160
Thomas Zwagerman <thomas_zwagerman@hotmail.com>
2022-09-21 17:19:15 +0100

    Consolidate code in server

--- R/poc_fdr.R
+++ R/poc_fdr.R
@@ -65,90 +65,94 @@ server <- function(input, output) {
     values_from = c("fixture", "rating")
   )
 
+  gw_input <- c(
+    c(9:16)
+  )
+
+  gw_columns <- glue::glue("fixture_{gw_input}")
+  rating_columns <- glue::glue("rating_{gw_input}")
+
+  # Create a totals column based on the number of gameweeks
+  totals <- table_by_gameweek |>
+    dplyr::select(team, all_of(rating_columns)) |>
+    tidyr::pivot_longer(
+      cols = rating_columns,
+      names_to = "gw",
+      values_to = "rating"
+    )
+
+  # Getting Average FDR to allow sorting for selected gameweeks.
+  # Filter out blank gameweeks and divide by number of games (rating),
+  # This is so blank gameweeks don't skew overall picture
+  avgs <- totals |>
+    group_by(team) |>
+    filter(rating > 0) |>
+    summarise(average_fdr = sum(rating, na.rm = TRUE) / length(rating))
+
+  avgs$average_fdr <- as.integer(avgs$average_fdr)
+
+  # Create a list of column definitions, as required
+  # by reactable
+  fpl_color_pal = c("#375523", "#01fc7a", "#808080", "#ff1751", "#80072d")
+
+  team_list <- list(
+    team = colDef(
+      maxWidth = 175,
+      align = 'left',
+      show = TRUE
+    )
+  )
+
+  rating_cols <- lapply(gw_input, function(gw) {
+    colDef(
+      maxWidth = 175,
+      style = color_scales(table_df,
+                           colors = fpl_color_pal)
+    )
+  })
+
+  # These need to be named lists!
+  names(rating_cols) <- rating_columns
+
+  fixture_cols <- lapply(gw_input, function(gw) {
+    colDef(
+      style = color_scales(table_df,
+                           colors = fpl_color_pal,
+                           color_by = glue::glue(
+                             "rating_{gw}"
+                           )
+      ),
+      sortable = FALSE
+    )
+  })
+
+  names(fixture_cols) <- gw_columns
+
+  avg_fdr_list <- list(
+    average_fdr = colDef(
+      maxWidth = 175
+    )
+  )
+
+  # Combine the lists into one list, and group them.
+  # This is so gw rating and fixture appear together
+  table_list <- c(team_list, fixture_cols, rating_cols, avg_fdr_list)
+
+  column_groups <- lapply(gw_input, function(gw) {
+    colGroup(name = glue::glue("Gameweek {gw}"), columns = c(
+      glue::glue("fixture_{gw}"),
+      glue::glue("rating_{gw}")
+    )
+    )
+  })
+
+  # Joining the totals, so  table can be sorted for selected GWs
+  table_by_gameweek <- left_join(table_by_gameweek, avgs)
+
+  table_df <- table_by_gameweek |>
+    select(team, all_of(gw_columns), average_fdr, all_of(rating_columns))
+
   output$table <- renderReactable({
-    gw_input <- c(
-      c(9:16)
-    )
-
-    gw_columns <- glue::glue("fixture_{gw_input}")
-    rating_columns <- glue::glue("rating_{gw_input}")
-
-    # Create a totals column based on the number of gameweeks
-    totals <- table_by_gameweek |>
-      dplyr::select(team, all_of(rating_columns)) |>
-      tidyr::pivot_longer(
-        cols = rating_columns,
-        names_to = "gw",
-        values_to = "rating"
-      )
-
-    totals$rating[is.na(totals$rating)] <- 0
-
-    totals <- totals |>
-      group_by(team) |>
-      summarise(total = sum(rating))
-
-    # Create a list of column definitions, as required
-    # by reactable
-    fpl_color_pal = c("#375523", "#01fc7a", "#808080", "#ff1751", "#80072d")
-
-    team_list <- list(
-      team = colDef(
-        maxWidth = 175,
-        align = 'left',
-        show = TRUE
-      )
-    )
-
-    rating_cols <- lapply(gw_input, function(gw) {
-      colDef(
-        maxWidth = 175,
-        style = color_scales(test,
-                             colors = fpl_color_pal)
-      )
-    })
-
-    # These need to be named lists!
-    names(rating_cols) <- rating_columns
-
-    fixture_cols <- lapply(gw_input, function(gw) {
-      colDef(
-        style = color_scales(test,
-                             colors = fpl_color_pal,
-                             color_by = glue::glue(
-                               "rating_{gw}"
-                             )
-        ),
-        sortable = FALSE
-      )
-    })
-
-    names(fixture_cols) <- gw_columns
-
-    total_list <- list(
-      total = colDef(
-        maxWidth = 175
-      )
-    )
-
-    # Combine the lists into one list, and group them.
-    # This is so gw rating and fixture appear together
-    table_list <- c(team_list, fixture_cols, rating_cols, total_list)
-
-    column_groups <- lapply(gw_input, function(gw) {
-      colGroup(name = glue::glue("Gameweek {gw}"), columns = c(
-        glue::glue("fixture_{gw}"),
-        glue::glue("rating_{gw}")
-      )
-      )
-    })
-
-    # Joining the totals, so  table can be sorted for selected GWs
-    table_by_gameweek <- left_join(table_by_gameweek, totals)
-
-    table_df <- table_by_gameweek |>
-      select(team, all_of(gw_columns), total, all_of(rating_columns))
-
     reactable(
       table_df,
       compact = TRUE,
